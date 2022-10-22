2b44c051cd7434e775395cd982b710194d166339
Ramith <Natakalaram1@gmail.com>
2022-10-22 14:18:58 +0530

    Added necessary files

---
+++ myapp/src/App.js
@@ -0,0 +1,42 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import {useState,useRef,useEffect} from 'react';
+import background from './assets/newyork.jpg';
+import Login from './login'
+
+import Register from './Register'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
+//exact is added to not display the home page even if the substring matches
+//in switch,only the first one matching is rendered 
+const App = () => {
+
+  return (
+
+    <Routes>
+   
+      <Route exact path='/register' element={<Register/>}/>
+        
+      <Route path="/login" element={<Login/>}/>
+        
+     <Route path="*" element={<h1>Error page does not exist</h1>}/>
+   
+    </Routes>
+  );
+
+}
+
+
+
+
+
+
+
+export default App;
+
+
+
+
+
---
+++ myapp/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App.js';
+import reportWebVitals from './reportWebVitals';
+import './fonts/Lato-Light.ttf';
+import Register from './Register'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
+
+ReactDOM.render(
+  <Router>
+    <App/>
+  </Router>,
+   document.getElementById("root")
+);  
+
+
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
