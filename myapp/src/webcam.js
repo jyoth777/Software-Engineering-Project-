245e55b34e994122d0ca1c5f55154800da9d5dc1
KarthickManikandan2003 <114656323+KarthickManikandan2003@users.noreply.github.com>
2022-11-16 19:24:46 +0530

    Add files via upload

---
+++ myapp/src/webcam.js
@@ -0,0 +1,68 @@
+import React, { Component, useState } from 'react';
+
+import Webcam from "react-webcam";
+
+
+
+const videoConstraints = {
+    width: 220,
+    height: 200,
+    facingMode: "user"
+};
+
+const WebcamCapture = () => {
+
+    const [image, setImage] = useState('');
+
+    const webcamRef = React.useRef(null);
+
+    const capture = React.useCallback(
+        () => {
+            const imageSrc = webcamRef.current.getScreenshot();
+            setImage(imageSrc)
+        },
+
+        [webcamRef]
+    );
+
+    return (
+        <div>
+            <div className="webcam-container">
+                <Webcam
+                    audio={false}
+                    height={200}
+                    ref={webcamRef}
+                    screenshotFormat="image/jpeg"
+                    width={220}
+                    videoConstraints={videoConstraints}
+                />
+                <button
+                    onClick={(e) => { e.preventDefault(); capture(); }}>
+                    Capture</button>
+            </div>
+            <div className="webcam-container">
+
+                <div className="webcam-img">
+
+                    {image == '' ? <Webcam
+
+                        audio={false}
+
+                        height={200}
+
+                        ref={webcamRef}
+
+                        screenshotFormat="image/jpeg"
+
+                        width={220}
+
+                        videoConstraints={videoConstraints}
+
+                    /> : <img src={image} />}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default WebcamCapture;
